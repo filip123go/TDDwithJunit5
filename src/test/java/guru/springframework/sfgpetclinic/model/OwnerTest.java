103c06ac3be963b8d5a06f6a68478bb01732a6a0
filippidis <filippidis@intralot.com>
2019-09-26 11:55:25 +0300

    74. JUnit Parameterized Tests - CSV File Source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,10 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import jdk.swing.interop.SwingInterOpUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.ParameterizedTest.DEFAULT_DISPLAY_NAME;
 
 class OwnerTest implements ModelTests {
 
@@ -23,7 +31,43 @@ class OwnerTest implements ModelTests {
 				() -> assertAll("Owner Properties",
 						() -> assertEquals("Key West", owner.getCity(), "-----City Did Not Match------"),
 						() -> assertEquals("1231231234", owner.getTelephone()),
-						() -> assertEquals("North Street 17" , owner.getAddress() , "-----City Did Not Match------")
+						() -> assertEquals("North Street 17", owner.getAddress(), "-----City Did Not Match------")
 				));
 	}
+
+	@DisplayName("Value Source Test - ")
+	@ParameterizedTest(name = "{displayName}" + DEFAULT_DISPLAY_NAME)
+	@ValueSource(strings = {"Spring", "Framework", "Guru"})
+	void testValueSource(String val) {
+		System.out.println(val);
+
+	}
+
+
+	@DisplayName("Enum Source Test - ")
+	@ParameterizedTest(name = "{displayName}" + DEFAULT_DISPLAY_NAME)
+	@EnumSource(OwnerType.class)
+		//iterate through the enumeration and pass each value in enumTest
+	void enumTest(OwnerType ownerType) {
+		System.out.println(ownerType);
+	}
+
+	@DisplayName("Csv Input Test - ")
+	@ParameterizedTest(name = "{displayName}" + DEFAULT_DISPLAY_NAME)
+	@CsvSource({
+			"FL, 1, 1",
+			"OH, 1, 2",
+			"MI, 3, 1"
+	})
+	void csvInputTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + "=" + val1 + ":" + val2);
+	}
+
+	@DisplayName("Csv From File Test - ")
+	@ParameterizedTest(name = "{displayName}" + DEFAULT_DISPLAY_NAME)
+	@CsvFileSource(resources = "/input.csv", numLinesToSkip = 1)
+	void csvFromFileTest(String stateName, int val1, int val2) {
+		System.out.println(stateName + "=" + val1 + ":" + val2);
+	}
+
 }
--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,31 +1,42 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.RepeatedTest.LONG_DISPLAY_NAME;
+import static org.junit.jupiter.api.RepeatedTest.SHORT_DISPLAY_NAME;
 
 class PersonTest implements ModelTests {
 
-    @Test
-    void groupedAssertions() {
-        //given
-        Person person = new Person(1l, "Joe", "Buck");
+	@Test
+	void groupedAssertions() {
+		//given
+		Person person = new Person(1l, "Joe", "Buck");
 
-        //then
-        assertAll("Test Props Set",
-                () -> assertEquals(person.getFirstName(), "Joe"),
-                () -> assertEquals(person.getLastName(), "Buck"));
-    }
+		//then
+		assertAll("Test Props Set",
+				() -> assertEquals(person.getFirstName(), "Joe"),
+				() -> assertEquals(person.getLastName(), "Buck"));
+	}
 
-    @Test
-    void groupedAssertionMsgs() {
-        //given
-        Person person = new Person(1l, "Joe", "Buck");
+	@Test
+	void groupedAssertionMsgs() {
+		//given
+		Person person = new Person(1l, "Joe", "Buck");
 
-        //then
-        assertAll("Test Props Set",
-                () -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
-                () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
-    }
+		//then
+		assertAll("Test Props Set",
+				() -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
+				() -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
+	}
+
+	@RepeatedTest(value = 10, name = LONG_DISPLAY_NAME)
+	@DisplayName("My repeated test")
+	@Test
+	void myRepeatedTest() {
+		//todo - impl
+	}
 }
